efa2aee05ddcfdce3012265bf9e5d0a36bee646a
Lucas Heredia <lucasheredia5475@gmail.com>
2022-12-28 11:19:46 -0300

    spring boot project

---
+++ TurnPoint/TurnPoint/src/main/java/com/turnpoint/TurnPoint/TurnPointApplication.java
@@ -0,0 +1,13 @@
+package com.turnpoint.TurnPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnPointApplication.class, args);
+	}
+
+}
---
+++ TurnPoint/TurnPoint/src/test/java/com/turnpoint/TurnPoint/TurnPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnpoint.TurnPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
