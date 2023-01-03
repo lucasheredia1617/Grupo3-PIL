62c2d33dc8019400d1e7da48f44df613276e2d2a
Nahuel Troncoso <nahueltroncosoraskovsky@gmail.com>
2023-01-03 19:15:41 -0300

    Adding classes (AdminService and AdminServiceImpl),  and one Interface (TurnPointRepository).

---
+++ src/main/java/controller/AdminController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.Administrator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import services.AdminService;
+
+import java.util.List;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    private AdminService AdminService;
+
+    @PostMapping("/admin")
+    public Administrator saveAdmin(@RequestBody Administrator admin) {
+        return AdminService.saveAdmin(admin);
+    }
+
+    // Read operation
+    @GetMapping("/admin")
+    public List<Administrator> listAdmin() {
+        return AdminService.listAdmin();
+    }
+
+    // Update operation
+    @PutMapping("/admin/{document}")
+    public Administrator
+    updatePersona(@RequestBody Administrator admin,
+                  @PathVariable("document") Long document) {
+        return AdminService.updateAdmin(
+                admin, document);
+    }
+
+    // Delete operation
+    @DeleteMapping("/admin/{document}")
+    public String deleteAdminByDoc(@PathVariable("document")
+                                   Long document) {
+        AdminService.deleteAdmin(
+                document);
+        return "Deleted Successfully";
+    }
+}
---
+++ src/main/java/model/Administrator.java
@@ -0,0 +1,12 @@
+package model;
+public class Administrator {
+
+    private Long document;
+
+    public Administrator(Long document, String name) {
+        this.document = document;
+        this.name = name;
+    }
+
+    String name = "Gustavo";
+}
